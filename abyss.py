52026c1dc7eb72ef307e25c0fcee46d7e3c4d524
ntddk <ntinternal@gmail.com>
2016-12-31 19:08:18 +0900

    multithreading

--- abyss.py
+++ abyss.py
@@ -11,10 +11,11 @@ import magic
 import os
 import argparse
 import sys
+import threading
 import logging
 from logging import getLogger, StreamHandler, DEBUG
 
-def fetch_soup(url):
+def fetch_soup(name, url):
     request = urllib2.Request(url)
     request.add_header('User-Agent', 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)')
 
@@ -22,26 +23,26 @@ def fetch_soup(url):
         html = urllib2.urlopen(request, timeout=10)
 
     except urllib2.URLError as e:
-        logger.error('-- {0}, fetching from {1}'.format(e.reason, url))
+        logger.error('{0}: {1}, fetching from {2}'.format(name, e.reason, url))
         return
 
     except Exception as e:
-        logger.error('- Failed to fetch from {0}'.format(url))
+        logger.error('{0}: Failed to fetch from {1}'.format(name, url))
         return
 
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-def fetch_file(url, dest_path):
+def fetch_file(name, url, dest_path):
     try:
         file_binary = urllib2.urlopen(url, timeout=10).read()
 
     except urllib2.URLError as e:
-        logger.error('-- {0}, fetching from {1}'.format(e.reason, url))
+        logger.error('{0}: {1}, fetching from {2}'.format(name, e.reason, url))
         return
 
     except Exception as e:
-        logger.error('- Failed to fetch from {0}'.format(url))
+        logger.error('{0}: Failed to fetch from {1}'.format(name, url))
         return
 
     filetype = magic.from_buffer(file_binary, mime=True).decode(sys.stdin.encoding).split(' ')[0]
@@ -56,41 +57,44 @@ def fetch_file(url, dest_path):
     if not os.path.exists(dest_file_path):
         with open(dest_file_path, 'wb') as f:
             f.write(file_binary)
-        logger.debug('-- Saved file type {0} with md5: {1}'.format(filetype, file_md5))
+        logger.debug('{0}: Saved file type {1} with md5: {2}'.format(name, filetype, file_md5))
 
 def malwaredl(soup, dest_path):
-    logger.debug('- Fetching from Malware Domain List')
+    name = sys._getframe().f_code.co_name
+    logger.debug('{0}: Fetching from Malware Domain List'.format(name))
     description_soup = soup('description')[1:]
 
-    logger.debug('-- Found {0} urls'.format(len(description_soup)))
+    logger.debug('{0}: Found {1} urls'.format(name, len(description_soup)))
 
     for xml in description_soup:
         url = 'http://' + xml.string.replace('&amp;', '&').split(',')[0][6:]
-        fetch_file(url, dest_path)
+        fetch_file(name, url, dest_path)
 
 def vxvault(soup, dest_path):
-    logger.debug('- Fetching from VXvault')
+    name = sys._getframe().f_code.co_name
+    logger.debug('{0}: Fetching from VXvault'.format(name))
 
     url_list = soup('pre')[0].string.replace('&amp;', '&').split('\r\n')[4:-1]
-    logger.debug('-- Found {0} urls'.format(len(url_list)))
+    logger.debug('{0}: Found {1} urls'.format(name, len(url_list)))
 
     for url in url_list:
-        fetch_file(url, dest_path)
+        fetch_file(name, url, dest_path)
 
 def malc0de(soup, dest_path):
-    logger.debug('- Fetching from Malc0de')
+    name = sys._getframe().f_code.co_name
+    logger.debug('{0}: Fetching from Malc0de'.format(name))
 
     description_soup = soup('description')[1:]
-    logger.debug('-- Found {0} urls'.format(len(description_soup)))
+    logger.debug('{0}: Found {1} urls'.format(name, len(description_soup)))
 
     for xml in description_soup:
         host = xml.string.replace('&amp;', '&').split(',')[0][5:]
         if host is not None:
             url = 'http://' + host
-            fetch_file(url, dest_path)
+            fetch_file(name, url, dest_path)
         else:
             ip_address = xml.text.split(',')[1][13:]
-            fetch_file('http://' + ip_address, dest_path)
+            fetch_file(name, 'http://' + ip_address, dest_path)
 
 if __name__ == '__main__':
     print('    ___  _                    _    _       _       _               ')
@@ -127,12 +131,18 @@ if __name__ == '__main__':
 
     import urllib2
 
-    logger.debug('- {0}'.format(today))
+    logger.debug('{0}: {1}'.format(__name__, today))
 
     try:
-        malwaredl(fetch_soup('http://www.malwaredomainlist.com/hostslist/mdl.xml'), dest_path)
-        vxvault(fetch_soup('http://vxvault.siri-urz.net/URL_List.php'), dest_path)
-        malc0de(fetch_soup('http://malc0de.com/rss'), dest_path)
+        t1 = threading.Thread(target = malwaredl, args = (fetch_soup(__name__, 'http://www.malwaredomainlist.com/hostslist/mdl.xml'), dest_path))
+        t2 = threading.Thread(target = vxvault, args = (fetch_soup(__name__, 'http://vxvault.siri-urz.net/URL_List.php'), dest_path))
+        t3 = threading.Thread(target = malc0de, args = (fetch_soup(__name__, 'http://malc0de.com/rss'), dest_path))
+        t1.start()
+        t2.start()
+        t3.start()
+        t1.join()
+        t2.join()
+        t3.join()
     except Exception, e:
-        logger.error('- {0}'.format(e))
+        logger.error('{0}: {1}'.format(__name__, e))
         pass
